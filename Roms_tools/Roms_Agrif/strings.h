375c3bb4e2d81f6902f60970cc3f7f4fc671fc8b
Gildas Cambon <gildas.cambon@ird.fr>
2013-03-20 14:29:37 +0000

    Copy the trunk in the spectra_tools branche
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/branches/spectra_tools@1093 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

---
+++ Roms_tools/Roms_Agrif/strings.h
@@ -0,0 +1,19 @@
+! $Id$
+!
+!======================================================================
+! ROMS_AGRIF is a branch of ROMS developped at IRD and INRIA, in France
+! The two other branches from UCLA (Shchepetkin et al) 
+! and Rutgers University (Arango et al) are under MIT/X style license.
+! ROMS_AGRIF specific routines (nesting) are under CeCILL-C license.
+! 
+! ROMS_AGRIF website : http://roms.mpl.ird.fr
+!======================================================================
+!
+! A long character string to hold activated cpp-switches.
+! Basically it is used to keep track of cpp-switches by placing
+! them together and writing into history file.
+!                                    !
+      integer max_opt_size           ! NOTE: Parameter max_opt_size
+      parameter (max_opt_size=2300)  ! must be equal to the length
+      character*2300 Coptions,srcs   ! of character string. 
+      common /strings/ Coptions,srcs !
